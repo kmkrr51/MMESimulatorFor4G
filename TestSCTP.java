5cac9c22dda3e7823f542469ab00df27e4d07f9b
kmkrr51 <133234620+kmkrr51@users.noreply.github.com>
2023-11-07 16:57:43 +0530

    Add files via upload

---
+++ TestSCTP.java
@@ -0,0 +1,8 @@
+package com.hpe.cms4g.cmb;
+
+public class TestSCTP
+{
+    public static void main(String[] args) throws Exception {
+        com.sun.nio.sctp.SctpChannel sc = com.sun.nio.sctp.SctpChannel.open();
+    }
+}
